8373b05ed308d74e32b27a4b9dba2cd01bb59d59
Rmarcial <rogerio.pires@lmit.pt>
2021-07-20 21:43:08 +0100

    added module and corrected some errors

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/goburrow/modbus"
+)
+
+// Modbus TCP constants
+const (
+	Ip = "192.168.1.19"
+	Id = 1
+)
+
+// HT20 16-bit register addresses
+const (
+	TemperatureAddr = 6000
+	HumidityAddr    = 6002
+	DewPointAddr    = 6004
+)
+
+func main() {
+
+	handler := modbus.NewTCPClientHandler("localhost:502")
+	handler.Timeout = 10 * time.Second
+	handler.SlaveId = byte(Id)
+
+	err := handler.Connect()
+	defer handler.Close()
+
+	client := modbus.NewClient(handler)
+
+	fmt.Println(err)
+	fmt.Println(client)
+
+	fmt.Println("HT20 readings:")
+
+}
