429966d070e92beb71e0f4142b9d0763e0d80664
Rmarcial <rogerio.pires@lmit.pt>
2021-07-21 01:52:20 +0100

    reading temperature done

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"time"
 
@@ -9,31 +11,54 @@ import (
 
 // Modbus TCP constants
 const (
-	Ip = "192.168.1.19"
-	Id = 1
+	ipAddr   = "192.168.1.20:502"
+	modbusId = 1
 )
 
-// HT20 16-bit register addresses
+// HT20 32-bit register addresses (contain floats in IEEE-754 standard. Order of bytes: B3 B2 B1 B0)
 const (
-	TemperatureAddr = 6000
-	HumidityAddr    = 6002
-	DewPointAddr    = 6004
+	TemperatureAddr = 7500
+	HumidityAddr    = 7501
+	DewPointAddr    = 7502
 )
 
 func main() {
 
-	handler := modbus.NewTCPClientHandler("localhost:502")
+	// Handler
+	handler := modbus.NewTCPClientHandler(ipAddr)
 	handler.Timeout = 10 * time.Second
-	handler.SlaveId = byte(Id)
+	handler.SlaveId = modbusId
 
 	err := handler.Connect()
 	defer handler.Close()
+	if err != nil {
+		println("Error connecting handler:", err.Error())
+	}
 
+	// Client
 	client := modbus.NewClient(handler)
-
-	fmt.Println(err)
 	fmt.Println(client)
 
+	// Read Measurements
+	bsTemp, err := client.ReadInputRegisters(TemperatureAddr, 2)
+	if err != nil {
+		fmt.Println("Error reading temperature", err.Error())
+	}
+
+	var temperature float32
+	var reader = bytes.NewReader(bsTemp)
+
+	if readErr := binary.Read(reader, binary.BigEndian, &temperature); readErr != nil {
+		fmt.Println("Error reading binary data")
+	}
+
+	temperature = float32(temperature)
+
+	// Print
 	fmt.Println("HT20 readings:")
+	fmt.Println()
+	fmt.Printf("Temperature (ºC): \t %v \n", temperature)
+	fmt.Printf("Relative Humidity (%%): \t ????\n")
+	fmt.Printf("Dew Point (ºC): \t ????\n")
 
 }
